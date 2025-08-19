59932c278c5a6df9f03fa0bf13b876471ea373f4
samyoub-2004 <oubrahamsamy19@gmail.com>
2025-08-19 13:12:38 +0100

    qsd

--- server/server.js
+++ server/server.js
@@ -4,7 +4,32 @@ const stripe = require('stripe')(process.env.API_STRIPE_KEY);
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+
+// Configuration CORS pour autoriser des URLs spécifiques
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Liste des URLs autorisées
+    const allowedOrigins = [
+      'https://priolys.onrender.com',
+      'https://www.votre-domaine.com',
+      'http://localhost:3000', // Pour le développement
+      'http://127.0.0.1:3000'  // Alternative localhost
+    ];
+    
+    // Autoriser les requêtes sans origine (comme Postman, curl, etc.)
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // Si vous avez besoin de cookies/authentification
+  optionsSuccessStatus: 200 // Pour les navigateurs plus anciens
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.post('/create-payment-intent', async (req, res) => {
